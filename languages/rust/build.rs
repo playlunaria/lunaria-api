08579641539137a8cf6241830468e233381d0f0f
Jan David <jandavid@playlunaria.com>
2020-10-17 11:49:19 +0200

    Publish packages when releasing a new version
    Automation has been set up to push new packages to npm and crates.io
    when a new release is published.

--- languages/rust/build.rs
+++ languages/rust/build.rs
@@ -1,12 +1,31 @@
+use std::path::PathBuf;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let root_path = find_project_root().unwrap();
+    let proto_path = root_path.join("protobufs");
+
     let build_server = std::env::var("CARGO_FEATURE_SERVER").is_ok();
 
     tonic_build::configure()
         .build_server(build_server)
         .compile(
-            &["../../protobufs/lunaria/v1/lunaria.proto"],
-            &["../../protobufs"],
+            &["lunaria/v1/lunaria.proto"],
+            &[proto_path.to_str().unwrap()],
         )?;
 
     Ok(())
+}
+
+fn find_project_root() -> Option<PathBuf> {
+    let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+
+    while let Some(parent) = path.parent() {
+        path = parent.to_path_buf();
+
+        if path.ends_with("lunaria-api") {
+            return Some(path);
+        }
+    }
+
+    None
 }
