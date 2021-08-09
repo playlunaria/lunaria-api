19be4b17fa28d2a7d900f382281f381923fc89d9
Jan David <jandavid@playlunaria.com>
2021-08-09 17:41:33 +0200

    Make command to start a game a synchronous API call
    The command to start a game has been changed to return a synchronous
    response in place of a server-side stream. This simplifies the necessary
    work to start a game, and reduces the barrier to entry for inexperienced
    developers.

--- test-server/src/game.rs
+++ test-server/src/game.rs
@@ -1,11 +1,5 @@
-use std::pin::Pin;
-
-use tokio::sync::mpsc::channel;
-use tokio_stream::wrappers::ReceiverStream;
-use tonic::codegen::futures_core::Stream;
 use tonic::{Request, Response, Status};
 
-use lunaria_api::lunaria::v1::start_game_response::GameStatus;
 use lunaria_api::lunaria::v1::{StartGameRequest, StartGameResponse};
 
 #[derive(Clone, Debug, Default)]
@@ -13,23 +7,10 @@ pub struct Game {}
 
 #[tonic::async_trait]
 impl lunaria_api::lunaria::v1::game_service_server::GameService for Game {
-    type StartGameStream =
-        Pin<Box<dyn Stream<Item = Result<StartGameResponse, Status>> + Send + Sync + 'static>>;
-
     async fn start_game(
         &self,
         _request: Request<StartGameRequest>,
-    ) -> Result<Response<Self::StartGameStream>, Status> {
-        let (tx, rx) = channel(4);
-
-        tx.send(Ok(StartGameResponse {
-            status: GameStatus::Running.into(),
-        }))
-        .await
-        .unwrap();
-
-        Ok(Response::new(
-            Box::pin(ReceiverStream::new(rx)) as Self::StartGameStream
-        ))
+    ) -> Result<Response<StartGameResponse>, Status> {
+        Ok(Response::new(StartGameResponse {}))
     }
 }
